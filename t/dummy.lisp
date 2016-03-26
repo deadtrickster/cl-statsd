1d3292510d64cf0ab6f82156698526607e01d44e
Ilya Khaprov <dead.trickster@gmail.com>
2016-03-26 01:03:30 -0700

    Tests

---
+++ t/dummy.lisp
@@ -0,0 +1,8 @@
+(in-package :cl-statsd.test)
+
+(plan 1)
+
+(subtest "Dummy, sanity check"
+  (ok :t "T is T"))
+
+(finalize)
---
+++ t/serializer.lisp
@@ -0,0 +1,29 @@
+(in-package :cl-statsd.test)
+
+(defmacro with-fake-client (&body body)
+  `(let ((statsd:*client* (statsd:make-fake-client))
+         (statsd:*random-range* 0))
+     ,@body
+     (statsd:fake-client-recv)))
+
+(plan 1)
+
+(subtest "Protocol serializer"
+  (subtest "Counter"
+    (is (with-fake-client (statsd:counter "app.example" 3)) "app.example:3|c")
+    (is (with-fake-client (statsd:counter "app.example" 3 :rate 0.23)) "app.example:3|c|@0.23"))
+  
+  (subtest "Guage"
+    (is (with-fake-client (statsd:guage "app.example" 3)) "app.example:3|g")
+    (is (with-fake-client (statsd:guage "app.example" 3 :rate 0.23)) "app.example:3|g|@0.23"))
+
+  (subtest "Set"
+    (is (with-fake-client (statsd:set "app.example" 3)) "app.example:3|s")
+    (is (with-fake-client (statsd:set "app.example" 3 :rate 0.23)) "app.example:3|s|@0.23"))
+
+  (subtest "Timing"
+    (is (with-fake-client (statsd:timing "app.example" 3)) "app.example:3|ms")
+    (is (with-fake-client (statsd:timing "app.example" 3 :rate 0.23)) "app.example:3|ms|@0.23")
+    (is (with-fake-client (statsd:with-timing ("app.example") (sleep 1.5))) "app.example:1500|ms")))
+
+(finalize)
