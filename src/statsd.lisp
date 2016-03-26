00a81f0db36ec4e7924f64a84ed6ad81e72ff30e
Ilya Khaprov <dead.trickster@gmail.com>
2016-03-26 00:49:41 -0700

    fix with-timing macro

--- src/statsd.lisp
+++ src/statsd.lisp
@@ -14,13 +14,17 @@
 (defun set (key value &key (rate) (client *client*))
   (send client :set key value rate))
 
-(defmacro with-timing ((&optional (client *client*)) &body body)
-  `(multiple-value-bind (sec1 nsec1) (local-time::%get-current-time)
-     ,@body
-     (multiple-value-bind (sec2 nsec2) (local-time::%get-current-time)
-       (let ((msec-d  (floor (- nsec2 nsec1) 1000000))
-             (sec-d (- sec2 sec1)))
-         (when (< msec-d 0)
-           (decf sec-d)
-           (incf msec-d 1000))
-         (timing key (+ sec-d 1000 msec-d) :client ,client)))))
+(defun with-timing% (client key lambda)
+  (multiple-value-bind (sec1 nsec1) (local-time::%get-current-time)
+     (multiple-value-prog1
+         (funcall lambda)
+       (multiple-value-bind (sec2 nsec2) (local-time::%get-current-time)
+         (let ((msec-d  (floor (- nsec2 nsec1) 1000000))
+               (sec-d (- sec2 sec1)))
+           (when (< msec-d 0)
+             (decf sec-d)
+             (incf msec-d 1000))
+           (timing key (+ (* sec-d 1000) msec-d) :client client))))))
+
+(defmacro with-timing ((key &optional (client '*client*)) &body body)
+  `(with-timing% ,client ,key (lambda () ,@body)))
